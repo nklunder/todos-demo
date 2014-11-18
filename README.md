0855cb87c11e0aa6f831c8c10580599e45de8755
Nicholas Klunder <nklunder@gmail.com>
2014-11-17 16:27:22 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 1-Hour Todo Demo App
+---
+## Features
+* Add task and description
+* Remove tasks from list
---
+++ todos-demo.js
@@ -0,0 +1,10 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
+    }
+  })
+
+}
