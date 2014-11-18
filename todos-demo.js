bcb1855e915f862bea663ee7548f8e865c2dfc61
Nicholas Klunder <nklunder@gmail.com>
2014-11-17 17:16:59 -0800

    add the final files

--- todos-demo.js
+++ todos-demo.js
@@ -3,8 +3,28 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, { sort: { createdOn: -1 } });
     }
-  })
+  });
 
+  Template.body.events({
+    "submit .new-task": function (event) {
+      var title = event.target.title.value;
+      var description = event.target.description.value;
+
+      Tasks.insert({
+        title: title,
+        description: description,
+        createdOn: new Date()
+      })
+
+      event.target.title.value = "";
+      event.target.description.value = "";
+
+      return false;
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
 }
